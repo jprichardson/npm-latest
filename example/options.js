f99e24158cd339c36f0aa0eeb0f3d48a1c3df593
Stefan Buck <web@broud.de>
2014-04-18 00:23:07 +0200

    add possibility to override the request options

---
+++ example/options.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var npmLatest = require('./../lib/npm-latest.js');
+
+// The request options. For more information please visit:
+// https://github.com/mikeal/request/#requestoptions-callback
+var options = {
+  url: 'https://registry.nodejitsu.com/%s',
+  timeout:1500
+};
+
+npmLatest('express', options, function (err, result) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(result.name + ' v'+ result.version);
+});
